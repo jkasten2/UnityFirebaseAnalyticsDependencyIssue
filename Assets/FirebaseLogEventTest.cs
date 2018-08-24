db4bae602af744efd1bcb7ae9bc6391c90e3c243
Josh Kasten <josh@onesignal.com>
2018-08-23 17:30:09 -0700

    Initial commit of issue

---
+++ Assets/FirebaseLogEventTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseLogEventTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Log an event with no parameters.
+		Firebase.Analytics.FirebaseAnalytics
+		  .LogEvent(Firebase.Analytics.FirebaseAnalytics.EventLogin);
+
+		// Log an event with a float parameter
+		Firebase.Analytics.FirebaseAnalytics
+		  .LogEvent("progress2", "percent", 0.4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
